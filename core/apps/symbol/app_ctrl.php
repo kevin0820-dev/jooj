4b52aca7445892f2d69d4938abb7d6c58ac170f2
kevin0820-dev <kevinallen5877066@gmail.com>
2024-10-04 13:54:38 -0400

    undefined array key fixed

--- core/apps/symbol/app_ctrl.php
+++ core/apps/symbol/app_ctrl.php
@@ -121,7 +121,8 @@ function cl_get_profile_posts($symbol_id = false, $limit = 30, $media = false, $
                 $post_data['is_quote'] = (($row['type'] == 'quote') ? true : false);
                 $post_data['comment_on'] = null;
                 if($post_data['is_quote']){
-					$post_data['comment_on'] = cl_get_guest_feed_one($row['comment_on'])[0];		/* edited by kevin to fetch comment on (added) */	
+					$post_data['comment_on'] = cl_get_guest_feed_one($row['comment_on']);		/* edited by kevin to fetch comment on (added) */	
+                    if(isset($post_data['comment_on'][0])) $post_data['comment_on'] = $post_data['comment_on'][0];
 				}
                 $data[] = cl_post_data($post_data);
             }
