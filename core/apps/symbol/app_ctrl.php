aeaa99f9ac125531de74bef5de151366a4f29166
kevin0820-dev <kevinallen5877066@gmail.com>
2024-10-14 04:14:01 -0300

    symbol page fixed

--- core/apps/symbol/app_ctrl.php
+++ core/apps/symbol/app_ctrl.php
@@ -59,7 +59,7 @@ function cl_get_profile_posts($symbol_id = false, $limit = 30, $media = false, $
                 }
 
                 if($post_data['is_quote']){
-					$post_data['comment_on'] = cl_get_guest_feed_one($row['comment_on'])[0];		/* edited by kevin to fetch comment on (added) */	
+					$post_data['comment_on'] = cl_get_guest_feed_one($row['comment_on']);		/* edited by kevin to fetch comment on (added) */	
                     if($post_data['comment_on']) $post_data['comment_on'] = $post_data['comment_on'][0];
 				}
                 
