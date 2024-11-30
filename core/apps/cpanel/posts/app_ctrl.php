c3ac8b3342e92fb12679c08d4c026b734ae081ef
kevin0820-dev <kevinallen5877066@gmail.com>
2024-11-29 22:49:19 -0300

    publication manager post content added

--- core/apps/cpanel/posts/app_ctrl.php
+++ core/apps/cpanel/posts/app_ctrl.php
@@ -43,12 +43,38 @@ function cl_admin_get_posts($args = array()) {
 
     if (cl_queryset($posts)) {
         foreach ($posts as $row) {
-            $row['url']           = cl_link(cl_strf('thread/%d', $row['id']));
-            $row['replys_count']  = cl_number($row['replys_count']);
-            $row['reposts_count'] = cl_number($row['reposts_count']);
-            $row['likes_count']   = cl_number($row['likes_count']);
-            $row['time']          = date('d M, Y h:m',$row['time']);
-            $data[]               = $row;
+            $post_data = cl_raw_post_data($row['id']);
+            $post_data['url']           = cl_link(cl_strf('thread/%d', $row['id']));
+            $post_data['replys_count']  = cl_number($row['replys_count']);
+            $post_data['reposts_count'] = cl_number($row['reposts_count']);
+            $post_data['likes_count']   = cl_number($row['likes_count']);
+            // $post_data['time']          = date('d M, Y h:m',$row['time']);
+            $post_data['is_repost']   = (($row['type'] == 'repost') ? true : false);
+            $post_data['is_quote']   = (($row['type'] == 'quote') ? true : false);		/* edited by kevin to quote comment */
+            $post_data['is_reposter'] = false;
+            $post_data['attrs']       = array();
+            $post_data['comment_on']  = null;						/* edited by kevin to fetch comment on (added) */
+
+            if ($post_data['is_repost']) {
+                $reposter_data         = cl_user_data($row['user_id']);
+                $post_data['reposter'] = array(
+                    'name' => $reposter_data['name'],
+                    'username' => $reposter_data['username'],
+                    'url' => $reposter_data['url'],
+                );
+            }
+
+            if($post_data['is_quote']){
+                $post_data['comment_on']  = cl_get_guest_feed_one($row['comment_on']);
+                if($post_data['comment_on']) $post_data['comment_on'] = $post_data['comment_on'][0];		/* edited by kevin to fetch comment on (added) */
+            }
+
+            if (isset($me['id']) && $row['user_id'] == $me['id']) {
+                $post_data['is_reposter'] = true;
+            }
+
+            $post_data['attrs'] = ((not_empty($post_data['attrs'])) ? implode(' ', $post_data['attrs']) : '');
+            $data[]             = cl_post_data($post_data);
         }
     }
     
